e13171ea2258ed7ee021a2470adfc901b542fb3a
Reza Dehghani <rdehghani.akorn@gmail.com>
2024-01-12 11:11:24 +0330

    student class was created

---
+++ StudentManagement/StudentManagement/Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement;
+public class Student
+{
+    public Student(string name, int studentId)
+    {
+        Name = name;
+        StudentId = studentId;
+    }
+    public string Name { get; set; }
+    public int StudentId { get; set; }
+}
+
+
+public class Course
+{
+    public Course(string name, int unit)
+    {
+        Name = name;
+        Unit = unit;
+    }
+    public string Name { get; set; }
+    public int Unit { get; set; }
+}
+
+public class ChoiceCourse : Student, IManagerChoiceCourse
+{
+    public ChoiceCourse(string name, int studentId, double corseGrade, string corseName, int courseUnit) : base(name, studentId)
+    {
+        CorseGrade = corseGrade;
+        CorseName = corseName;
+        this.courseUnit = courseUnit;
+    }
+
+    public double CorseGrade { get; set; }
+    public string CorseName { get; set; }
+    public int courseUnit { get; set; }
+
+
+
+    public void EditStudentGrade(double courseGrade)
+    {
+        CorseGrade = courseGrade;
+    }
+
+
+}
+
+public interface IManagerChoiceCourse
+{
+
+    void EditStudentGrade(double courseGrade);
+
+}
